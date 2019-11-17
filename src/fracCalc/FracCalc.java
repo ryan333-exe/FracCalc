12c99ac36ec4fdbb982d85da6672e8ee7f67db7d
ryan333-exe <s145520@scusd.net>
2019-11-16 18:06:11 -0800

    checkpoint 3 and most of final checkpoint complete

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -9,7 +9,7 @@ public class FracCalc {
         System.out.print("Enter an expression: ");
         String in = input.nextLine();
         while (!in.equalsIgnoreCase("quit")) {
-            produceAnswer(in);
+            System.out.println(produceAnswer(in));
             System.out.print("Enter an expression: ");
             in = input.nextLine();
         }
@@ -30,44 +30,142 @@ public class FracCalc {
         String firstOp = input.substring(0, (input.indexOf(" ")));
         String Operator = input.substring(input.indexOf(" ") + 1, input.indexOf(" ") + 2);
         String secondOp = input.substring(input.indexOf(" ") + 3, input.length());
+        String retStr = null;
+        int [] frac1 = fraction(firstOp);
+        int [] frac2 = fraction(secondOp);
 
-        fractionString(firstOp);
+        //if the operator is a plus sign
+        if (Operator.equals("+")) {
+            int den3 = frac1[1] * frac2[1];
+            int num3 = (frac1[0] * frac2[1]) + (frac2[0] * frac1[1]);
+            int gcd = GCD(num3, den3);
+            den3 = den3 / gcd;
+            num3 = num3 / gcd;
 
-        return fractionString(secondOp);
+            retStr = makeMixed(num3, den3);
+        }
 
+        //if the operator is a minus sign
+        if (Operator.equals("-")) {
+            int den3 = frac1[1] * frac2[1];
+            int num3 = (frac1[0] * frac2[1]) - (frac2[0] * frac1[1]);
+            int gcd = GCD(num3, den3);
+            den3 = den3 / gcd;
+            num3 = num3 / gcd;
 
+            retStr = makeMixed(num3, den3);
+        }
+
+        //if the operator is a times (multiplication) sign
+        if (Operator.equals("*")) {
+            int num3 = frac1[0] * frac2[0];
+            int den3 = frac1[1] * frac2[1];
+            int gcd = GCD(num3, den3);
+            den3 = den3 / gcd;
+            num3 = num3 / gcd;
+
+            retStr = makeMixed(num3, den3);
+        }
+
+        //if the operator is a division sign
+        if (Operator.equals("/")) {
+            int num3 = frac1[0] * frac2[1];
+            int den3 = frac1[1] * frac2[0];
+
+            int gcd = GCD(num3, den3);
+            den3 = den3 / gcd;
+            num3 = num3 / gcd;
+
+            if (den3 < 0) {
+                den3 = den3 * -1;
+                num3 = num3 * -1;
+            }
+
+            retStr = makeMixed(num3, den3);
+        }
+
+        return retStr;
         // TODO: Implement this function to produce the solution to the input
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
 
-    public static String fractionString(String input) {
-        int x = 0;
-        int y = 0;
-        int z = 1;
+    public static int[] fraction(String input) {
+        int wholeNum = 0;
+        int numerator = 0;
+        int denominator = 1;
         int wholePosition;
         int fracSep;
+
         wholePosition = input.indexOf('_');
         fracSep = input.indexOf('/');
+
         //if only a whole number exists
         if ((wholePosition == -1) && (fracSep == -1)) {
-            x = Integer.parseInt(input);
-            y = 0;
-            z = 1;
+            wholeNum = Integer.parseInt(input);
+            numerator = 0;
+            denominator = 1;
         }
         //if both a whole number and fraction exist
         if ((wholePosition > 0) && (fracSep > 0)) {
-            x = Integer.parseInt(input.substring(0, wholePosition));
-            y = Integer.parseInt(input.substring(wholePosition + 1, fracSep));
-            z = Integer.parseInt(input.substring(fracSep + 1));
+            wholeNum = Integer.parseInt(input.substring(0, wholePosition));
+            numerator = Integer.parseInt(input.substring(wholePosition + 1, fracSep));
+            denominator = Integer.parseInt(input.substring(fracSep + 1));
         }
         //if only a fraction exists
         if ((wholePosition == -1) && (fracSep > 0)) {
-            x = 0;
-            y = Integer.parseInt(input.substring(0, fracSep));
-            z = Integer.parseInt(input.substring(fracSep + 1));
+            wholeNum = 0;
+            numerator = Integer.parseInt(input.substring(0, fracSep));
+            denominator = Integer.parseInt(input.substring(fracSep + 1));
         }
-        return ("whole:" + x + " numerator:" + y + " denominator:" + z);
+
+        //performs calculation (multiplying denominator by whole number, then adding it to the numerator making it the new numerator.)
+        //finally put the new numerator over the old denominator
+        int [] impFrac = new int[2];
+        if (wholeNum >= 0) {
+            int tempValue = denominator * wholeNum;
+            numerator = numerator + tempValue;
+            impFrac[0] = numerator;
+            impFrac[1] = denominator;
+        }
+        else {
+            int tempValue = denominator * wholeNum;
+            numerator = tempValue - numerator;
+            impFrac[0] = numerator;
+            impFrac[1] = denominator;
+        }
+        return (impFrac);
+    }
+
+    public static int GCD(int a, int b) {
+        if (b == 0) {
+            return Math.abs(a);
+        }
+        return GCD(b, a % b);
+    }
+
+    public static String makeMixed(int a, int b) {
+        String retStr = null;
+        if (a == 0) {
+            return "0";
+        }
+        if (a / b != 0) {
+            int wholeNum = a / b;
+            int tempVal = (Math.abs(a)) % (Math.abs(b));
+            if (tempVal == 0) {
+                return ("" + wholeNum + "");
+            }
+            if (wholeNum != 0) {
+                retStr = ("" + wholeNum + "_" + tempVal + "/" + b + "");
+            } else {
+                retStr = ("" + tempVal + "/" + b + "");
+            }
+        }
+        else {
+            retStr = ("" + a +"/" + b + "");
+        }
+
+        return retStr;
     }
 }
 
